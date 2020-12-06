dfe7439268128004e0e27f933ff8f01398dc2929
yasith_sam <yasith.sam7@gmail.com>
2020-12-06 13:39:25 +0530

    Movie db initialized

---
+++ Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace movieedb.Models
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+       
+        public int Id { get; set; }
+
+
+    }
+}
